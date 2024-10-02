499dfd1f40eea0c325a357dbfca776adcb870bc8
Harish <harishchaurasia143@gmail.com>
2024-10-02 11:20:44 -0700

    sign in button improvement

--- src/components/GoogleSigninButton.tsx
+++ src/components/GoogleSigninButton.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
-import { signInWithGoogle, signOut } from '../services/firebase';
-import { FirebaseAuthTypes } from '@react-native-firebase/auth';
-
+import React, {useState} from 'react';
+import {
+  Button,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {signInWithGoogle, signOut} from '../services/firebase';
+import {FirebaseAuthTypes} from '@react-native-firebase/auth';
 
 const GoogleSignInButton = () => {
   const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
@@ -29,11 +35,17 @@ const GoogleSignInButton = () => {
     <View style={styles.container}>
       {user ? (
         <>
-          <Text>Welcome, {user.displayName ? user.displayName : "User"}</Text>
+          <Text>Welcome, {user.displayName ? user.displayName : 'User'}</Text>
           <Button title="Sign Out" onPress={handleSignOut} />
         </>
       ) : (
-        <Button title="Sign in with Google" onPress={handleSignIn} />
+        <TouchableOpacity style={styles.googleButton} onPress={handleSignIn}>
+          <Image
+            source={require('../assets/google.png')}
+            style={styles.googleIcon}
+          />
+          <Text style={styles.googleButtonText}>Continue with Google</Text>
+        </TouchableOpacity>
       )}
     </View>
   );
@@ -42,9 +54,33 @@ const GoogleSignInButton = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center', // Center the content vertically
-    alignItems: 'center', // Center the content horizontally
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 20,
+  },
+  googleButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  googleIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 12,
+    marginLeft: 10,
+  },
+  googleButtonText: {
+    fontSize: 16,
+    color: '#000',
+    marginHorizontal: 10,
+    marginVertical: 6,
   },
 });
 
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,14 +1,12 @@
 // src/screens/LoginScreen.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 import GoogleSignInButton from '../components/GoogleSigninButton';
 import Bckgrnd from '../components/GradientBackground';
 const LoginScreen = () => {
   return (
     <Bckgrnd>
       <View>
-        <Text>Join</Text>
-        <Text>LocalVendor</Text>
         <GoogleSignInButton />
       </View>
     </Bckgrnd>
